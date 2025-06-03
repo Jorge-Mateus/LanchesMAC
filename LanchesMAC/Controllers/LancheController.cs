6743dde3e2bdc6e87fc6500583fc51b67e0a0b9c
Jorge Mateus <69761413+Jorge-Mateus@users.noreply.github.com>
2025-06-02 22:11:24 -0300

    pedidos, pedidos details, migrations, alteracoes de layout

--- LanchesMAC/Controllers/LancheController.cs
+++ LanchesMAC/Controllers/LancheController.cs
@@ -48,5 +48,32 @@ namespace LanchesMAC.Controllers
             return View(lanche);
         }
 
+        public ViewResult Search(string searchString)
+        {
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(searchString))
+            {
+                lanches = _lancheRepositRepository.Lanches.OrderBy(p => p.LancheId);
+                categoriaAtual = "Todos os Lanches";
+            }
+            else
+            {
+                lanches = _lancheRepositRepository.Lanches
+                          .Where(p => p.Nome.ToLower().Contains(searchString.ToLower()));
+
+                if (lanches.Any())
+                    categoriaAtual = "Lanches";
+                else
+                    categoriaAtual = "Nenhum lanche foi encontrado";
+            }
+
+            return View("~/Views/Lanche/List.cshtml", new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            });
+        }
     }
 }
---
+++ LanchesMAC/Repositories/Interface/IPedidoRepository.cs
@@ -0,0 +1,9 @@
+﻿using LanchesMAC.Models;
+
+namespace LanchesMAC.Repositories.Interface
+{
+    public interface IPedidoRepository
+    {
+        void CriarPedido(Pedido pedido);
+    }
+}
